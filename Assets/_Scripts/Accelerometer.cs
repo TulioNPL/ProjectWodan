a10cf78b840e2fb8f82a019e03d610d3b343ee2f
TulioNPL <tulionp.lopes@gmail.com>
2019-06-09 13:26:11 -0300

    Adicionado MainMenu

---
+++ Assets/_Scripts/Accelerometer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public bool isFlat = true;
+    private float speed = 10.0f;
+    private Rigidbody rigid;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 tilt = Input.acceleration * speed;
+
+        if (isFlat)
+        {
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+
+        rigid.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.green);
+    }
+}
