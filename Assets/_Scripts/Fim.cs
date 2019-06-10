7cd8d029d1d6e0958f1f56b2ed3d0950aaaa3c0b
TulioNPL <tulionp.lopes@gmail.com>
2019-06-10 11:41:31 -0300

    WinScreen adicionada

---
+++ Assets/_Scripts/Fim.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fim : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("End"))
+        {
+            Debug.Log("LOLOLOLOLOLOLOL");
+            SceneManager.LoadScene("WinMenu");
+        }
+    }
+}
--- Assets/_Scripts/WinScreen.cs
+++ Assets/_Scripts/WinScreen.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class IngameMenu : MonoBehaviour {
+public class WinScreen : MonoBehaviour {
     public void voltarAoMenu() {
         SceneManager.LoadScene("Menu");
     }
